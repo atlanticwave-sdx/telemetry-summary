b4c753c8e00a1577426debbbe0b4228a7db11126
Arturo Quintana <arturocu81@gmail.com>
2021-03-08 10:55:50 -0500

    - Inclusion of new tests related to the Telemetry Packet class (packet refresher method and packet builder)

--- tests/test_telemetry_packet_class.py
+++ tests/test_telemetry_packet_class.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
 
-import pytest
-import requests_mock
-from influxdb import InfluxDBClient
 from mock import MagicMock, patch
 
 from src.influxdb_manager import InfluxDB
@@ -30,10 +27,32 @@ class TestTelemetryPacketClass(object):
         assert telemetry_packet.sources['amlight_sdx_1']['l2vpn_bra_usa_11242']['sequence_number'] == 12345
 
     def test_packet_refresher(self):
-        pass
+        test_args = ['test', '-f', 'config_test.ini']
+
+        with patch.object(sys, 'argv', test_args):
+            setting = Setting()
+            setting.read_params()
+            influxdb = InfluxDB(setting)
+            influxdb.open_connection()
+
+            with patch('random.randint', MagicMock(return_value=12345)):
+                telemetry_packet = TelemetryPacket(influxdb, setting)
+                telemetry_packet.packet_builder()
+                telemetry_packet.packet_refresher()
+
+        assert telemetry_packet.sources['amlight_sdx_2']['l2vpn_bra_usa_22242']['out_octets'] == 12346
 
     def test_packet_builder(self):
-        pass
+        test_args = ['test', '-f', 'config_test.ini']
 
-    def test_packet_sender(self):
-        pass
+        with patch.object(sys, 'argv', test_args):
+            setting = Setting()
+            setting.read_params()
+            influxdb = InfluxDB(setting)
+            influxdb.open_connection()
+
+            with patch('random.randint', MagicMock(return_value=12345)):
+                telemetry_packet = TelemetryPacket(influxdb, setting)
+                telemetry_packet.packet_builder()
+
+        assert telemetry_packet.packet_info['fields']['optimizations_counter'] == 12345
